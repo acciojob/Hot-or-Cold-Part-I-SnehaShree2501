db11ce0cfc557bcab30f97f446352da0a7688fb8
sneha shree <snehashree2501@gmail.com>
2023-03-19 14:46:38 +0530

    Cold or Hot, part 1

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 //your code here
+var btn=document.getElementById("btn");
+var randomNum;
+function generateRandomNumber(){
+    alert("Button Clicked");
+     randomNum= (Math.random * 40)-20;
+    findGuessCloseToNumber();
+}
+
+function findGuessCloseToNumber()
+{
+     input=Number(document.querySelector("guess").value);
+    if((Math.abs(input-randomNum))>=5)
+    {
+        document.querySelector("respond").textContent="Cold";
+    }
+    else
+    {
+        document.querySelector("respond").textContent="Hot";
+    }
+}
+btn.addEventListener("click",generateRandomNumber);
 
