849d7d659eb1fcf990467253e3a4e4d9f5bdc2c4
sneha shree <snehashree2501@gmail.com>
2023-03-19 18:56:47 +0530

    Cold or Hot, part 1

--- script.js
+++ script.js
@@ -1,23 +1,42 @@
 //your code here
-var btn=document.getElementById("btn");
-var randomNum;
-function generateRandomNumber(){
-    alert("Button Clicked");
-     randomNum= (Math.random * 40)-20;
-    findGuessCloseToNumber();
-}
+const respond = document.querySelector('#respond');
+const randomNumber =Math.floor(Math.random() * 50 );
+console.log(randomNumber)
 
-function findGuessCloseToNumber()
-{
-     input=Number(document.querySelector("guess").value);
-    if((Math.abs(input-randomNum))>=5)
-    {
-        document.querySelector("respond").textContent="Cold";
-    }
-    else
-    {
-        document.querySelector("respond").textContent="Hot";
-    }
-}
-btn.addEventListener("click",generateRandomNumber);
+let guessTry = 1;
+let previousDiff = 0;
 
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', guessTheDiff);
+
+function guessTheDiff(){
+    let guess = document.querySelector('#guess');
+    if(guessTry <2){
+        if(guess.value == randomNumber){
+            respond.textContent = 'Success'
+        }
+        else if(guess.value <randomNumber){
+            respond.textContent = 'Guess higher'
+        }
+        else{
+            respond.textContent = 'guess lower'
+        }
+    }
+    else{
+        let diff = guess.value - randomNumber;
+        if(guess.value == randomNumber){
+            respond.textContent = 'Success'
+        }
+        else if(diff < previousDiff){
+            respond.textContent = 'Getting hotter';
+        }
+        else if(guess.value < randomNumber){
+            respond.textContent = 'guess higher'
+        }
+        else{
+            respond.textContent = 'guess lower'
+        }
+    }
+    previousDiff = guess.value - randomNumber;
+    guessTry++;
+}
